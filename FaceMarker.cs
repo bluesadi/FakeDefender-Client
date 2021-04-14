0867c24170d5b4f53d371fa576e48e2afb2ccc41
bluesadi <liuyibo@stu.scu.edu.cn>
2021-04-14 23:37:11 +0800

    update

--- FaceMarker.cs
+++ FaceMarker.cs
@@ -16,7 +16,7 @@ namespace FakeDefender_Client {
             Scalar color = fakeProb > 0.5 ? Scalar.Red : Scalar.Green;
             Cv2.Rectangle(this.targetMat, facePtr1, facePtr2, color, 3);
             Point textPtr = new Point(facePtr1.X, facePtr2.Y + 22);
-            Cv2.PutText(this.targetMat, "Fake: " + fakeProb, textPtr, HersheyFonts.HersheyDuplex, 1, color, 2);
+            Cv2.PutText(this.targetMat, "Fake: " + String.Format("{0:N2}", fakeProb), textPtr, HersheyFonts.HersheyDuplex, 1, color, 2);
         }
     }
 }
---
+++ PageMain.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using OpenCvSharp.Extensions;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using Point = OpenCvSharp.Point;
+using System.Threading;
+using Window = System.Windows.Window;
+using OpenCvSharp;
+
+namespace FakeDefender_Client {
+    /// <summary>
+    /// PageMain.xaml 的交互逻辑
+    /// </summary>
+    public partial class PageMain : Page {
+
+
+        public PageMain() {
+            InitializeComponent();
+        }
+
+        private Bitmap CaptureScreen() {
+            //double w = SystemParameters.PrimaryScreenWidth;
+            //double h = SystemParameters.PrimaryScreenHeight;
+            double w = 2560;
+            double h = 1440;
+            Bitmap bitmap = new Bitmap((int)w, (int)h);
+            using (Graphics graphics = Graphics.FromImage(bitmap)) {
+                graphics.CopyFromScreen(0, 0, 0, 0, new System.Drawing.Size((int)w, (int)h));
+            }
+            return bitmap;
+        }
+
+        private void Minimize() {
+            Window window = Window.GetWindow(this);
+            window.WindowState = WindowState.Minimized;
+            window.Visibility = Visibility.Hidden;
+        }
+
+        private string SendPredictRequest(string jsonText) {
+            HttpWebRequest request = null;
+            HttpWebResponse response = null;
+            string result = null;
+
+            request = (HttpWebRequest)WebRequest.Create("http://127.0.0.1:5000/predict");
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            byte[] data = Encoding.UTF8.GetBytes(jsonText);
+            request.ContentLength = data.Length;
+            using (Stream reqStream = request.GetRequestStream()) {
+                reqStream.Write(data, 0, data.Length);
+                reqStream.Close();
+            }
+
+            response = (HttpWebResponse)request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            //获取响应内容
+            using (StreamReader reader = new StreamReader(stream, Encoding.UTF8)) {
+                result = reader.ReadToEnd();
+            }
+
+            return result;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            Minimize();
+            Thread.Sleep(500);
+            Mat screenShot = BitmapConverter.ToMat(CaptureScreen());
+            try {
+
+                string jsonText = NetUtil.BuildPacket(Guid.NewGuid().ToString(), screenShot);
+                string result = SendPredictRequest(jsonText);
+                DetectionResult detectionResult = DetectionResult.fromJson(result);
+                foreach (Face face in detectionResult.faces) {
+                    Point facePtr1 = new Point(face.x1, face.y1);
+                    Point facePtr2 = new Point(face.x2, face.y2);
+                    double fakeProb = face.score;
+                    FaceMarker faceMarker = new FaceMarker(screenShot);
+                    faceMarker.draw(facePtr1, facePtr2, fakeProb);
+                }
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+            Window window = Window.GetWindow(this);
+            window.WindowState = WindowState.Normal;
+            Cv2.ImShow("After", screenShot);
+            Cv2.WaitKey(0);
+        }
+    }
+
+}
